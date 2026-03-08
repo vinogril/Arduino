49c510ad116c6116c9d173ace28f721889fd6731
Ilia Vinogradov <ilia.vinogradov12@gmail.com>
2026-03-08 11:49:37 +0100

    Added counter

--- HeisserDrahtHome/HeisserDrahtHome.ino
+++ HeisserDrahtHome/HeisserDrahtHome.ino
@@ -1,13 +1,32 @@
 const int pinLed = 13;
 const int pinBtn = 12;
 
+size_t counter = 0;
+bool isTouching = false;
+
 void setup()
 {
   pinMode(pinLed, OUTPUT);
   pinMode(pinBtn, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  digitalWrite(pinLed, digitalRead(pinBtn) == HIGH ? LOW : HIGH);
+  if (digitalRead(pinBtn) == LOW)
+  {
+    if (!isTouching)
+    {
+      ++counter;
+      Serial.println(counter);
+      delay(300);
+    }
+      isTouching = true;
+    digitalWrite(pinLed, HIGH);
+  }
+  else
+  {
+    isTouching = false;
+    digitalWrite(pinLed, LOW);
+  }
 }
